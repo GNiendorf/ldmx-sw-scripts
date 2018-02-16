28a130b526f9a4dd995c6e4dee1faf8e6947a4b3
omar-moreno <email@omarmoreno.net>
2018-02-16 10:30:15 -0800

    Update so files not containing the ROOT tree of interest get trashed.

--- utils/file_crawler.py
+++ utils/file_crawler.py
@@ -5,9 +5,14 @@ import sys
 import ROOT as r
 import os
 
+from rootpy.io import root_open
+from rootpy.io import DoesNotExist
+
 def main() : 
    
-    # Parse all command line arguments using the argparse module
+    # Parse all command line arguments using the argparse module. All that
+    # is currently required is the directory containing the files to crawl
+    # and the name of the ROOT tree containing the data.
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-d', action='store', dest='dir', 
                         help='Directory containing the list of files to crawl.')
@@ -16,20 +21,33 @@ def main() :
     args = parser.parse_args()
 
     if not args.dir:
-        parser.error('Please specify a path to the files that will be processed.')
-   
+        parser.error('Please specify a path to the files to be processed.')
+  
+    # Get the list of all files and directories and filter out the directory
+    # names. 
+    files = (file for file in os.listdir(args.dir.strip()) 
+                     if os.path.isfile(os.path.join(args.dir.strip(), file)))
+
+    total_files = 0
     total_entries = 0
-    for file_name in os.listdir(args.dir): 
+    for file_name in files: 
         print 'Crawling file %s' % file_name.strip()
-        root_file = r.TFile('%s/%s' % (args.dir, file_name.strip()))
-        tree = root_file.Get(args.tree.strip())
-        if tree is None: 
-            print 'File is empty.'
+        root_file = root_open('%s/%s' % (args.dir.strip(), file_name.strip()))
+        try:
+            tree = root_file.Get(args.tree.strip())
+        except DoesNotExist:
+            print 'Moving %s to trash.' % file_name.strip()
+            if not os.path.exists('%s/trash' % args.dir.strip()):
+                os.makedirs('%s/trash' % args.dir.strip())
+            root_file.Close()
+            os.rename('%s/%s' % (args.dir.strip(), file_name.strip()), 
+                        '%s/trash/%s' % (args.dir.strip(), file_name.strip()))
             continue
         total_entries += tree.GetEntries()
+        total_files += 1
         root_file.Close()
 
-    print "Total number of entries %s" % total_entries
+    print 'Finished crawling a total number of %s files containing %s entries' % (total_files, total_entries)
 
 if __name__ == "__main__":
     main()
